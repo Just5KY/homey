f12b4c68cfd0be8ae09fefa89556462bf2a4ed9d
vlfldr <91982627+vlfldr@users.noreply.github.com>
2021-10-25 20:17:16 -0400

    Initial commit; skeletons of cards

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+      <h1>Greetings planet! I'm learning Vue.js!<br>This will be my badass lightweight home dashboard.</h1>
+    </div>
+    <div class="card-container">
+    <div v-for="srv in services" :key=srv.text>
+      <ServiceCard :name=srv.name :url=srv.url :icon=srv.icon></ServiceCard>
+    </div>
+    </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import ServiceCard from './components/ServiceCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    ServiceCard,
+  },
+  data: function () {
+    return {
+      services: [
+        { name: 'Whoogle', 
+          url: 'https://mntn.top',
+          icon: 'whoogle.png',
+        },
+        {
+          name: 'Navidrome', 
+          url: 'http://music.mntn.top',
+          icon: 'navidrome.png',
+        },
+        {
+          name: 'Nextcloud', 
+          url: 'https://dokk.duckdns.org',
+          icon: 'nextcloud.png',
+        },
+      ],
+    }
+  },
 }
 </script>
 
@@ -20,7 +48,16 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #a3a7ac;
   margin-top: 60px;
+}
+
+.card-container {
+  width: 50%;
+  margin: 0 auto;
+}
+
+body {
+  background-color: #3a4652;
 }
 </style>
