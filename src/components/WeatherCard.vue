3ff08b737b62905e2c1455e4e9b33d09f1149135
vlfldr <bullettime29@aol.com>
2021-12-07 18:31:52 -0500

    New screenshot

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="weather-card-container">
     <h2>Weather</h2>
-    <div v-for="w in weatherData.slice(0, 12)" :key="w.time" class="weatherElement" >{{w.time}}<span>{{w.temp}}F {{w.weather_type}}</span></div>
+    <div v-for="w in weatherData" :key="w.time" class="weatherElement" >{{w.time}}<span>{{w.temp}}F {{w.weather_type}}</span></div>
   </div>
 </template>
 
@@ -19,7 +19,7 @@ export default {
   methods: {
     getHourlyWeather: function() {
         this.axios.get('http://0.0.0.0:9101/weatherHourly/20211207').then((res) => {
-          this.weatherData = res.data
+          this.weatherData = res.data.slice(0,10)
       }).catch(e => {
         console.log('Could not reach homey API');
       });
