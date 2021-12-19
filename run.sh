92c8b60a0f005fdeacd1470d671ae2df88f8e3dd
vlfldr <bullettime29@aol.com>
2021-12-19 14:37:08 -0500

    fix dangling python process in run script

--- run.sh
+++ run.sh
@@ -1,10 +1,20 @@
 #! /bin/bash
 
-# install requirements & run locally
+# local launch script for development/debugging
 # the app will not launch unless .env and homey-api/disks.txt are configured correctly
 
+# install requirements
 cd homey
 npm i
 pip install -r ../homey-api/requirements.txt
 
-python ../homey-api/app.py & npm run dev && kill $!
+# run backend; capture process ID for exit
+python ../homey-api/app.py &
+pid[0]=$!
+# run frontend
+npm run dev &
+pid[1]=$!
+
+# kill both on interrupt
+trap "kill ${pid[0]} ${pid[1]}; exit 1" INT
+wait
