1d684e633deb2b735f09854b3d0eb49872d89a6d
vlfldr <bullettime29@aol.com>
2022-01-02 23:30:23 -0500

    Swapped to trois.js

--- homey/src/App.vue
+++ homey/src/App.vue
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <button @click="addService" id="add-box-btn">Add</button>
-    <ThreeCanvas :data="boxes" />
+    <ThreeCanvas :boxes="this.boxes" :height="500" :width="500" />
   </html>
 </template>
 <script>
@@ -22,8 +22,17 @@ export default {
   },
   methods: {
     addService() {
-      this.boxes.push({"name": "Service", "URL": "https://yandex.ru"});
+      this.boxes.push({"name": "Test Service", "URL": "https://yandex.ru"});
     },
+  },
+  mounted() {
+    this.addService();
   },
 }
 </script>
+
+<style scoped>
+  html {
+    grid-column: 2;
+  }
+</style>
--- homey/src/components/ThreeCanvas.vue
+++ homey/src/components/ThreeCanvas.vue
@@ -1,12 +1,43 @@
 <template>
-    <canvas id="docker-canvas"></canvas>
+  <Renderer ref="renderer" :alpha="true" :antialias="true" :resize="true">
+    <Camera :position="{ z: 10 }" />
+    <Scene>
+      <PointLight :position="{ y: 50, z: 50 }" />
+      <Group ref="boxGroup">
+        <Box v-for="b in this.boxes" :key="b.name" :ref="'box' + boxes.indexOf(b)" 
+          :position="{ x: boxes.indexOf(b) }"
+          :size=".8">
+          <LambertMaterial />
+        </Box>
+      </Group>
+    </Scene>
+  </Renderer>
 </template>
 
 <script>
 export default {
-    name: 'ThreeCanvas',
-    props: {
-        data: Array,
-    },
+  name: 'ThreeCanvas',
+  props: {
+    width: Number,
+    height: Number,
+    boxes: Array,
+  },
+  mounted() {
+    const renderer = this.$refs.renderer;
+    renderer.three.setSize(this.width, this.height);
+    const boxGroup = this.$refs.boxGroup;
+
+    renderer.onBeforeRender(() => {
+      // spin
+      for(let i = 0; i < this.boxes.length; i++){
+        boxGroup.o3d.children[i].rotation.y += 0.01;
+      }
+      
+    });
+  },
 }
 </script>
+
+<style scoped>
+  
+</style>
