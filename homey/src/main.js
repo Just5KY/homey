3f5609ea272d5df14f19d15ee467bf1e54832e53
vlfldr <bullettime29@aol.com>
2022-01-17 02:21:29 -0500

    Remove unused deps in preparation for merge

--- homey/src/main.js
+++ homey/src/main.js
@@ -1,9 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import { TroisJSVuePlugin } from 'troisjs';
-
 // Entrypoint
 const app = createApp(App)
-app.use(TroisJSVuePlugin);
 app.mount('#app')
